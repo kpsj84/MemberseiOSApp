9e92846648940f584e368bf3661ba8a6ee4564a7
Kamal Jhinjer <kpsj84>
2022-08-08 13:37:45 +0530

    upto iWidget Screen Swipe Test case fine tune

--- src/test/java/Memberse/IOSAutomation/iWidgetSwipeTest.java
+++ src/test/java/Memberse/IOSAutomation/iWidgetSwipeTest.java
@@ -38,7 +38,7 @@ public class iWidgetSwipeTest extends IBase {
 			
 			//Verify the Text available on Login Option Page
 			Assert.assertEquals(Text, "Login");
-			System.out.println("Swipe Action and Click Action functionality on Widgets screen are working as Expected");
+			System.out.println("Swipe Action and Click Action functionality on Widget screens are working as Expected");
 		}
 
 }
